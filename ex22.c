38281c65b3a7577a089973cdef918ec840e298ee
Guy Adani <guy3102@gmail.com>
2022-05-13 02:21:14 +0300

    backup

--- ex22.c
+++ ex22.c
@@ -13,6 +13,8 @@
 #define WRITETONEWFILE O_WRONLY | O_CREAT | O_TRUNC | O_APPEND
 #define MAXSIZE 150
 
+// TODO: b.out
+
 // Writes error
 void writeError(char* errStr) {
     char str[11 + MAXSIZE] = "Error in: ";
@@ -49,11 +51,12 @@ void writeToCsv(int fd, int num, char* name) {
     
 }
 
-// TODO: close open files
 // Closes the file
-void closeFile(inputFD) {
-    if(close(inputFD)) {
-        writeError("close");
+void closeFiles(int inputFD[5]) {
+    int i;
+//    for(i = 0; i < 2; i++) closedir(inputFD[i]);
+    for(i = 2; i < 5; i++){
+        if(inputFD[i] != 0) close(inputFD[i]);
     }
 }
 
@@ -64,7 +67,7 @@ int compileFile (char* path, char* name) {
     int childPD, retVal;
     if((childPD = fork()) < 0) writeError("fork");
     if (childPD == 0) {
-        execlp("gcc", "gcc", path, NULL);
+        execlp("gcc", "gcc", "-o", "compiled.out", path, NULL);
         writeError("execlp");
         exit(-1);
     }
@@ -76,7 +79,7 @@ int compileFile (char* path, char* name) {
 
 // Runs the executable file
 int runFile(char* input) {
-    int inputFD, outputFD, childPD, retVal;
+    int inputFD, outputFD, childPD;
 
     // Set I/O redirection
     if ((dup2((outputFD = open("output.txt", WRITETONEWFILE, 0644)) , 1)) == -1){
@@ -91,7 +94,7 @@ int runFile(char* input) {
     // Run
     if ((childPD = fork()) < 0) writeError("fork");
     if (childPD == 0) {
-        execlp("./a.out", "./a.out", NULL);
+        execlp("./compiled.out", "./compiled.out", NULL);
         writeError("execlp");
         exit(-1);
     }
@@ -122,29 +125,36 @@ int checkOutput(char* correct){
 }
 
 int main(int argc, char *argv[]) {
-    int configurationFD, resultsFD, compResult, flag;   // Config. and results file FD, result of comp, flag for csv
+    int configurationFD, resultsFD;                     // Config. and results file FD, errors FD
+    int compResult, flag;                               // result of comp, flag for csv
     char configuraionContent[3][MAXSIZE];               // The input from config. file
     char c;                                             // Used to read config. file
     DIR *dirStr, *inDirStr;                             // The dir streams
     struct dirent *dit, *inDit;                         // The dir iterators
     struct stat dis;                                    // Used to check the inputs
-    char path[MAXSIZE];
+    char path[MAXSIZE];                                 // Used to use paths
+    int fds[3] = {0, 0, 0};                             // 0 errors 1 results 2 conf
 
     // Set error file
-    if ((dup2((open("./errors.txt", WRITETONEWFILE, 0644)) , 2)) == -1) {
+    if ((dup2((fds[0] = open("errors.txt", WRITETONEWFILE, 0644)) , 2)) == -1) {
         writeError("dup2");
-    }
-
-    if((resultsFD = open("results.csv", WRITETONEWFILE, 0644)) == -1) {
-        writeError("open");
+        closeFiles(fds);
         return -1;
     }
+
+    // Open results file
+    if((resultsFD = open("results.csv", WRITETONEWFILE, 0644)) == -1) {
+        writeError("open");
+        closeFiles(fds);
+        return -1;
+    } else fds[1] = resultsFD;
 
     // Open the configuration file
     if((configurationFD = open(argv[1], O_RDONLY)) == -1) {
         writeError("open");
+        closeFiles(fds);
         return -1;
-    }
+    } else fds[2] = configurationFD;
 
     // Read the configuration file content
     int i, j;
@@ -162,47 +172,58 @@ int main(int argc, char *argv[]) {
     // Check the configuration file data
     if (stat(configuraionContent[0], &dis)) {
         write(2, "Not a valid directory\n", 22);
+        closeFiles(fds);
         return -1;
     }
     if (!S_ISDIR(dis.st_mode)) {
         write(2, "Not a valid directory\n", 22);
+        closeFiles(fds);
         return -1;
     }
     if (access(configuraionContent[1], F_OK) != 0) {
         write(2, "Input file not exist\n", 21);
+        closeFiles(fds);
         return -1;
     }
     if (access(configuraionContent[2], F_OK) != 0) {
         write(2, "Output file not exist\n", 22);
+        closeFiles(fds);
         return -1;
     }
 
     // Open stream to the directory from the configuration file
     if((dirStr = opendir(configuraionContent[0])) == NULL) {
         writeError("opendir");
+        closeFiles(fds);
         return -1;
     }
 
+    // Start going through the student's dirs
     while((dit = readdir(dirStr)) != NULL) {
         // TODO: readdir check
 
-        if (!strcmp(dit->d_name, ".") || !strcmp(dit->d_name, "..")) continue;
-        if (dit->d_type != DT_DIR) continue;
+        // If . or .. or not a dir - next file
+        if (!strcmp(dit->d_name, ".") || !strcmp(dit->d_name, "..") || dit->d_type != DT_DIR) continue;
 
+        // Open student's dir
         strcpy(path, configuraionContent[0]);
         strcat(path, "/");
         strcat(path, dit->d_name);
-        if ((inDirStr = opendir(path)) == NULL) continue;
+        if ((inDirStr = opendir(path)) == NULL) {
+            closedir(inDirStr);
+            continue;
+        }
 
         // Check for each dir inside the main dir
         flag = 0;
         while ((inDit = readdir(inDirStr)) != NULL) {
-            if (!strcmp(inDit->d_name, ".") || !strcmp(inDit->d_name, "..")) continue;
-            if (inDit->d_type == DT_DIR) continue;
+            if (!strcmp(inDit->d_name, ".") || !strcmp(inDit->d_name, ".." ) || inDit->d_type == DT_DIR) continue;
+
+            // Check if .c file, if it is - grading it
             int len = strlen(inDit->d_name);
             if (inDit->d_name[len - 1] == 'c' && inDit->d_name[len - 2] == '.') {
                 flag = 1;
-                if((compileFile(path, inDit->d_name)) != 0) {
+                if((compileFile(path, inDit->d_name)) != 0) {       // Compilation error
                     writeToCsv(resultsFD, -1 ,dit->d_name);
                 } else {
                     runFile(configuraionContent[1]);
@@ -213,21 +234,15 @@ int main(int argc, char *argv[]) {
                 break;
             }
         }
+
+        // No c file
         if(!flag) writeToCsv(resultsFD, 0 ,dit->d_name);
+
+        closedir(inDirStr);
     }
 
-
-
-    // Closes the streams and files
-    if(closedir(dirStr) == -1) {
-        writeError("closedir");
-        return -1;
-    }
-    if(closedir(inDirStr) == -1) {
-        writeError("closedir");
-        return -1;
-    }
-
-
+    // Close files
+    closedir(dirStr);
+    closeFiles(fds);
     return 0;
 }
